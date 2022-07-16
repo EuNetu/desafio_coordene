8f7528f89a88faa979c46b86b6e78540ddaff754
neto <euneto2501@gmail.com>
2022-07-16 01:09:45 -0300

    form component structure created

---
+++ code/src/components/FormTransport.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-form-select v-model="selected" :options="options" size="sm" class="mt-3"></b-form-select>
+    <div class="mt-3">Selected: <strong>{{ selected }}</strong></div>
+  </div>
+</template>
+
+<script>
+  import {BFormSelect} from 'bootstrap-vue'
+
+  
+  export default {
+    components: {
+      BFormSelect
+    },
+    data() {
+
+      return {
+        selected: null,
+        options: [
+          { value: null, text: 'Por favor, selecione a cidade desejada' },
+          { value: 'Natal', text: 'Natal' },
+          { value: 'Recife', text: 'Recife' },
+          { value: 'São Paulo', text: 'São Paulo' },
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
